4f29cfe4e26ca6cea411a7e8ac250962ed6c4301
dmitry <dmitry@dv-m.ru>
2024-01-28 22:10:53 +0300

    add notify to handler

--- handlers/responseHandler.js
+++ handlers/responseHandler.js
@@ -1,6 +1,13 @@
 export const messageHandler = async (res) => {
   const jsonMessage = await res.clone().json();
   if ("message" in jsonMessage) {
+    UIkit.notification({
+      message: jsonMessage.message,
+      status: "primary",
+      pos: "bottom-center",
+      timeout: 5000,
+    });
+
     console.log(jsonMessage.message); //responseHandler
   }
 };
